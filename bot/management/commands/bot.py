8a0c40cddb2842e16eeed17da3630e757aff3990
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2022-09-23 17:58:55 +0300

    Refactor to 3 functions

--- bot/management/commands/bot.py
+++ bot/management/commands/bot.py
@@ -6,76 +6,80 @@ from telegram.ext import ApplicationBuilder, filters, MessageHandler
 
 from bot.models import Bot, Channel
 
+channels = list(Channel.objects.all())
+
+
+async def repost(update, context):
+    for channel in channels:
+        is_text_only = bool(update.channel_post.text_html)
+        is_text_with_image = bool(update.channel_post.caption_html)
+
+        if is_text_only:
+            work_content = update.channel_post.text_html
+        elif is_text_with_image:
+            work_content = update.channel_post.caption_html
+        else:
+            work_content = None
+
+        if is_text_only:
+            content = update_content(channel, work_content)
+            await context.bot.send_message(
+                chat_id=channel.telegram_id,
+                text=content,
+                parse_mode="HTML"
+            )
+
+        elif is_text_with_image:
+            content = update_content(channel, work_content)
+            await context.bot.copy_message(
+                chat_id=channel.telegram_id,
+                message_id=update.effective_message.id,
+                from_chat_id=update.effective_chat.id,
+                caption=content,
+                parse_mode="HTML"
+            )
+
+        else:
+            await context.bot.copy_message(
+                chat_id=channel.telegram_id,
+                message_id=update.effective_message.id,
+                from_chat_id=update.effective_chat.id
+            )
+
+
+def update_content(channel, work_content):
+    external_link_regex = "(http(s?)://[a-zA-Z0-9./=?#-]+)"
+    pin_link_regex = "(https://t.me/)"
+
+    content = work_content.replace(
+        channel.username_replacement[0], channel.username_replacement[1]).replace(
+        channel.promocode_replacement[0], channel.promocode_replacement[1])
+    if re.search(external_link_regex, content) is not None:
+        if re.search(pin_link_regex, content):
+            content = content.replace(
+                re.search('(https://t.me/[a-zA-Z\d/]+)', content).group(), channel.pin_message_link
+            )
+        else:
+            content = content.replace(
+                re.search(external_link_regex, content).group(), channel.external_link
+            )
+    return content
+
+
+def run_bot():
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO
+    )
+    enabled_bots = Bot.objects.filter(enabled=True)
+    token = enabled_bots.get(name=settings.BOT_NAME).token
+
+    application = ApplicationBuilder().token(token).build()
+    repost_handler = MessageHandler(filters.ALL, repost)
+    application.add_handler(repost_handler)
+    application.run_polling()
+
 
 class Command(BaseCommand):
-
-    @staticmethod
-    def update_content(channel, work_content):
-        external_link_regex = "(http(s?)://[a-zA-Z0-9./=?#-]+)"
-        pin_link_regex = "(https://t.me/)"
-
-        content = work_content.replace(
-            channel.username_replacement[0], channel.username_replacement[1]).replace(
-            channel.promocode_replacement[0], channel.promocode_replacement[1])
-        if re.search(external_link_regex, content) is not None:
-            if re.search(pin_link_regex, content):
-                content = content.replace(
-                    re.search('(https://t.me/[a-zA-Z\d/]+)', content).group(), channel.pin_message_link
-                )
-            else:
-                content = content.replace(
-                    re.search(external_link_regex, content).group(), channel.external_link
-                )
-        return content
-
     def handle(self, *args, **options):
-        logging.basicConfig(
-            format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-            level=logging.INFO
-        )
-        enabled_bots = Bot.objects.filter(enabled=True)
-        token = enabled_bots.get(name=settings.BOT_NAME).token
-
-        channels = list(Channel.objects.all())
-
-        async def repost(update, context):
-            for channel in channels:
-                is_text_only = bool(update.channel_post.text_html)
-                is_text_with_image = bool(update.channel_post.caption_html)
-
-                if is_text_only:
-                    work_content = update.channel_post.text_html
-                elif is_text_with_image:
-                    work_content = update.channel_post.caption_html
-                else:
-                    work_content = None
-
-                if is_text_only:
-                    content = self.update_content(channel, work_content)
-                    await context.bot.send_message(
-                        chat_id=channel.telegram_id,
-                        text=content,
-                        parse_mode="HTML"
-                        )
-
-                elif is_text_with_image:
-                    content = self.update_content(channel, work_content)
-                    await context.bot.copy_message(
-                        chat_id=channel.telegram_id,
-                        message_id=update.effective_message.id,
-                        from_chat_id=update.effective_chat.id,
-                        caption=content,
-                        parse_mode="HTML"
-                        )
-
-                else:
-                    await context.bot.copy_message(
-                        chat_id=channel.telegram_id,
-                        message_id=update.effective_message.id,
-                        from_chat_id=update.effective_chat.id
-                        )
-
-        application = ApplicationBuilder().token(token).build()
-        repost_handler = MessageHandler(filters.ALL, repost)
-        application.add_handler(repost_handler)
-        application.run_polling()
+        run_bot()
