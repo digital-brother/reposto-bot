c1ca2a9b08deb049f23cc60e486a29cf402b4470
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2022-09-23 17:12:57 +0300

    Add comment for Channel.title

--- bot/models.py
+++ bot/models.py
@@ -11,14 +11,16 @@ class Bot(models.Model):
 
 
 class Channel(models.Model):
-    bot = models.ForeignKey(Bot, on_delete=models.DO_NOTHING, default=None, related_name='channels')
-    title = models.CharField(max_length=100, blank=True)
+    bot = models.ForeignKey(Bot, on_delete=models.DO_NOTHING, related_name='channels')
     telegram_id = models.IntegerField()
 
-    username_replacement = models.JSONField(default=list)
-    promocode_replacement = models.JSONField(default=list)
+    username_replacement = models.JSONField()
+    promocode_replacement = models.JSONField()
     external_link = models.CharField(max_length=100, blank=True)
     pin_message_link = models.CharField(max_length=100, blank=True)
+
+    # Used for convenience in admin
+    title = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
         return self.title
