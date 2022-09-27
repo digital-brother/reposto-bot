3193faa05637a27d55f6add4368267c7d72555eb
Andrey <xi.forwork.ix@gmail.com>
2022-09-27 13:25:58 +0300

    Heroku staticfiles fix test

--- core/settings.py
+++ core/settings.py
@@ -128,9 +128,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/staticfiles/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "staticfiles"),
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
