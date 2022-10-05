f2da1c2ec271af0da91a3d8f88c4214eb154e211
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2022-10-06 00:44:44 +0300

    Reorder Channel fields

--- bot/models.py
+++ bot/models.py
@@ -13,14 +13,12 @@ class Bot(models.Model):
 class Channel(models.Model):
     bot = models.ForeignKey(Bot, on_delete=models.DO_NOTHING, related_name='channels')
     telegram_id = models.IntegerField()
+    title = models.CharField(max_length=100)  # Used for convenience in admin
 
     username_replacement = models.JSONField()
     promocode_replacement = models.JSONField()
     external_link = models.CharField(max_length=100, blank=True)
     pin_message_link = models.CharField(max_length=100, blank=True)
 
-    # Used for convenience in admin
-    title = models.CharField(max_length=100, blank=True)
-
     def __str__(self):
-        return self.title
+        return self.telegram_id
