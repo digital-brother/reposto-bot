0a53340c92153fa160e24ce6170cb5916a15343a
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2022-10-06 01:23:26 +0300

    Add username_replacement_field_name and promocode_replacement_field_name validation

--- bot/models.py
+++ bot/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 
@@ -19,6 +20,26 @@ class Channel(models.Model):
     promocode_replacement = models.JSONField()
     external_link = models.CharField(max_length=100, blank=True)
     pin_message_link = models.CharField(max_length=100, blank=True)
+
+    def clean(self):
+        username_replacement_field_name = Channel.username_replacement.field.verbose_name.capitalize()
+        self.clean_list_of_lists(self.username_replacement, username_replacement_field_name)
+
+        promocode_replacement_field_name = Channel.promocode_replacement.field.verbose_name.capitalize()
+        self.clean_list_of_lists(self.promocode_replacement, promocode_replacement_field_name)
+
+    @staticmethod
+    def clean_list_of_lists(content, field_name):
+        if not isinstance(content, list):
+            raise ValidationError(f"{field_name} should be a list")
+
+        for item in content:
+            if not isinstance(item, list):
+                raise ValidationError(f"{field_name} should contain a list of lists: {item} is not a list")
+
+            if not len(item) == 2:
+                raise ValidationError(f"Each {field_name} item should contain exactly 2 elements: "
+                                      f"{item} has invalid length")
 
     def __str__(self):
         return self.title
