0202fbac00b6c31dc0f15785ec12b230b5c1a88b
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2022-10-11 17:36:55 +0300

    Add ChannelAdmin

--- bot/admin.py
+++ bot/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
 
-from bot.models import Bot, Channel
+from bot.models import Bot, Channel, UsernameReplacement, PromocodeReplacement
+
+
+class UsernameReplacementInline(admin.TabularInline):
+    model = UsernameReplacement
+
+
+class PromocodeReplacementInline(admin.TabularInline):
+    model = PromocodeReplacement
+
+
+class ChannelAdmin(admin.ModelAdmin):
+    inlines = [
+        UsernameReplacementInline,
+        PromocodeReplacementInline
+    ]
+
 
 admin.site.register(Bot)
-admin.site.register(Channel)
+admin.site.register(Channel, ChannelAdmin)
